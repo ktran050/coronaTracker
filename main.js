394cbd62903fafcbe3f6f2fe53cb448614b1cc56
Kevin Tran <ktran050@gmail.com>
2020-04-13 15:32:58 -0700

    loaded select options

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
 // "use strict";
 const baseURL = "https://corona.lmao.ninja/";
 
-function loadCountryList() {
-  let resultsString = "";
-  fetch(baseURL + "v2/all")
+function drawCountryList() {
+  // called at the start of the script
+  let interList = [];
+  let resultHTML = [];
+  fetch(baseURL + "v2/countries")
     .then(function (result) {
       return result.text();
     })
     .then(function (result) {
-      resultsString = result;
-      console.log(resultsString);
+      interList = JSON.parse(result);
+      for (let i = 0; i < interList.length; ++i) {
+        resultHTML += `<option>${interList[i].country}</option>`;
+      }
+      $("#countriesList").html(resultHTML);
     });
-  console.log("list of countries loaded");
+  console.log("list of countries added");
 }
 
 function handleAddCountry() {
+  // called by handleEverything
   $("#optionsDiv").on("click", "#addCountry", function (event) {
     event.preventDefault();
-    console.log("add country handled");
+    let readInput = $("#countriesList").val();
+    console.log("country to add: ", readInput);
   });
 }
 
 function handleEverything() {
   handleAddCountry();
-  loadCountryList();
+  drawCountryList();
   console.log("everything handled");
 }
 
